e4fdb6200b661c39cae1ca9a7faee262a7b2021c
Luiz <luizprosdoskimi@gmail.com>
2023-05-09 16:10:29 -0300

    adicionando logo e os navlinks no header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,20 @@
 import { HeaderContainer } from "./styles";
 
+import logoIgnite from "../../assets/logo-ignite.svg";
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
 export const Header = () => {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logoIgnite} alt="logo do Ignite" />
       <nav>
-        <img src="" alt="" />
-        <a href="">Timer</a>
-        <a href="">History</a>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="historico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   );
